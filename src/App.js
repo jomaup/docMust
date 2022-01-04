d423a36dc8c8b38c052ac89195b37e0d4a553afc
Tidharus <tidharus.ae4@gmail.com>
2022-01-04 10:57:43 +0100

    footer et actu css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Header from "./Header";
 
+import Footer from "./Footer";
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+
+          
+
+      </main>
+      <Footer 
+    
+      />
     </div>
   );
 }
