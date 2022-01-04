9e4191dfd6ba8f47ad02f3bf5fc89627cc57c1c0
jomaup <maupas.joseph@yahoo.fr>
2022-01-04 11:15:26 +0100

    test sidebar

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar";
+import Documentation from "./Documentation"
+
+const Content = () => {
+    return (
+    <div className="container">
+        <Sidebar />
+        <Documentation />
+    </div>
+    )
+}
+
+export default Content
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,21 @@
-import ""
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
-
+                • Sidebar
+            </div>
+            <div className="sidebarWrapper">
+               • Sidebar
+            </div>
+            <div className="sidebarWrapper">
+               • Sidebar
+            </div>
+            <div className="sidebarWrapper">
+               • Sidebar
+            </div>
+            <div className="sidebarWrapper">
+               • Sidebar
             </div>
         </div>
     )
